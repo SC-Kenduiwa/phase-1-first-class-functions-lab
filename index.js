14bea9de3611cb6081e6404c3c7b4b0aed7c9d2e
Shanice Chebet Kenduiwa <kenduiwashanice63@gmail.com>
2024-03-22 13:34:34 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+/*console.log(returnFirstTwoDrivers(['Antonia', 'Nuru','Amari','Mo']));*/
+//function to return last two drivers
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+//selecting drivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+//higher-order function to create a fare multiplier
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return fare *integer;
+    };
+};
+//function to double the fare
+const fareDoubler =createFareMultiplier(2);
+
+//function to tripple the fare 
+const fareTripler =createFareMultiplier(3);
+
+//function to select different drivers based on thr provided function
+
+const selectDifferentDrivers = function(drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+};
+//Test cases 
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+console.log(fareDoubler(10));//output: 20
+console.log(fareTripler(10));//output: 30
+console.log(selctingDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'],returnFirstTwoDrivers));
+console.log(selctingDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'],returnLastTwoDrivers));
+
